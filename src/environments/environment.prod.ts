8037b3a71001d985f2ea4f21bce516e4dff07ad1
rosasthomas <rosasthomas@gmail.com>
2020-05-20 21:43:15 -0300

    config 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBlU-lAX4IMASwSlLRjiPrjsPgMQf5rkyw",
+    authDomain: "clinica-online-f8f32.firebaseapp.com",
+    databaseURL: "https://clinica-online-f8f32.firebaseio.com",
+    projectId: "clinica-online-f8f32",
+    storageBucket: "clinica-online-f8f32.appspot.com",
+    messagingSenderId: "842592483897",
+    appId: "1:842592483897:web:4e7bb3ec5130997cd8ebe8",
+    measurementId: "G-7XGHVSVGV3"
+  }
 };
